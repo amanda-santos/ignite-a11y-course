83a471be82016fd795f50456a63f4c994ef235b1
amanda-santos <amandasf0200@gmail.com>
2022-10-21 16:05:08 -0300

    fix: ♿️ add landmarks

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,8 +7,8 @@ import githubLogo from "../assets/github.svg";
 
 export default function Home() {
   return (
-    <div>
-      <div className={styles.header}>
+    <>
+      <header className={styles.header}>
         <Image src={rocketseatLogo} width={286 / 2} alt="" />
 
         <div className={styles.nav}>
@@ -16,33 +16,45 @@ export default function Home() {
             <Image src={githubLogo} width={24} alt="" />
           </a>
         </div>
-      </div>
+      </header>
 
-      <div className={styles.content}>
-        <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit</h2>
+      <main>
+        <article className={styles.content}>
+          <header>
+            <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit</h2>
 
-        <h4>
-          Repellat cupiditate in veniam sit maiores iste magni perferendis
-          commodi blanditiis perspiciatis, amet sed deserunt ullam voluptas
-          repellendus voluptatibus quibusdam
-        </h4>
+            <h4>
+              Repellat cupiditate in veniam sit maiores iste magni perferendis
+              commodi blanditiis perspiciatis, amet sed deserunt ullam voluptas
+              repellendus voluptatibus quibusdam
+            </h4>
+          </header>
 
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure
-          similique omnis nisi vero, necessitatibus odio qui beatae totam modi
-          officia provident quisquam, et eos corporis maiores, quam voluptatum
-          earum autem?
-        </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure
+            similique omnis nisi vero, necessitatibus odio qui beatae totam modi
+            officia provident quisquam, et eos corporis maiores, quam voluptatum
+            earum autem?
+          </p>
 
-        <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facilis
-          tenetur architecto amet quae magnam culpa! Sunt atque assumenda ea,
-          modi dolorum dicta suscipit repudiandae ab sapiente omnis iste. Magni,
-          fuga?
-        </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facilis
+            tenetur architecto amet quae magnam culpa! Sunt atque assumenda ea,
+            modi dolorum dicta suscipit repudiandae ab sapiente omnis iste.
+            Magni, fuga?
+          </p>
 
-        <h3>Temporibus, nobis possimus?</h3>
-      </div>
-    </div>
+          <h3>Temporibus, nobis possimus?</h3>
+        </article>
+      </main>
+
+      <footer className={styles.header} aria-label="footer">
+        <Image src={rocketseatLogo} width={286 / 2} alt="" />
+
+        <div className={styles.nav}>
+          <a href="https://github.com/amanda-santos">About us</a>
+        </div>
+      </footer>
+    </>
   );
 }
