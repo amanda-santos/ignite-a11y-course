fcb237aa4a5377ee32c3dec4cfa359e9d376d35a
amanda-santos <amandasf0200@gmail.com>
2022-10-21 15:50:46 -0300

    chore: ♿️ setup axe

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,15 @@
 import Head from "next/head";
+import { useEffect } from "react";
+
+import { axeAccessibilityReporter } from "../utils/axeAccessibilityReporter";
 
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    axeAccessibilityReporter();
+  });
+
   return (
     <>
       <Head>
