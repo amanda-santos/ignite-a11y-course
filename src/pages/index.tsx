03c916ed00d583db7dd6e74b1be4de9541ef1301
amanda-santos <amandasf0200@gmail.com>
2022-10-21 15:17:43 -0300

    feat: ✨ add Home structure

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,48 @@
+import Image from "next/future/image";
+
+import styles from "../styles/Home.module.css";
+
+import rocketseatLogo from "../assets/logo.svg";
+import githubLogo from "../assets/github.svg";
+
 export default function Home() {
   return (
-    <h1>Hello world</h1>
-  )
+    <div>
+      <div className={styles.header}>
+        <Image src={rocketseatLogo} width={286 / 2} alt="" />
+
+        <div className={styles.nav}>
+          <a href="https://github.com/amanda-santos">
+            <Image src={githubLogo} width={24} alt="" />
+          </a>
+        </div>
+      </div>
+
+      <div className={styles.content}>
+        <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit</h2>
+
+        <h4>
+          Repellat cupiditate in veniam sit maiores iste magni perferendis
+          commodi blanditiis perspiciatis, amet sed deserunt ullam voluptas
+          repellendus voluptatibus quibusdam
+        </h4>
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure
+          similique omnis nisi vero, necessitatibus odio qui beatae totam modi
+          officia provident quisquam, et eos corporis maiores, quam voluptatum
+          earum autem?
+        </p>
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facilis
+          tenetur architecto amet quae magnam culpa! Sunt atque assumenda ea,
+          modi dolorum dicta suscipit repudiandae ab sapiente omnis iste. Magni,
+          fuga?
+        </p>
+
+        <h3>Temporibus, nobis possimus?</h3>
+      </div>
+    </div>
+  );
 }
